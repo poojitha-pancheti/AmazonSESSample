dd80fcc602320c8996306f346c626c63fd53ca64
DPU J S <DPU J S@DESKTOP-12TU7QI>
2020-11-10 18:29:32 +0530

    Add project files.

---
+++ AmazonSESSample/Program.cs
@@ -0,0 +1,97 @@
+﻿using Amazon;
+using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+using System;
+using System.Collections.Generic;
+
+namespace AmazonSESSample
+{
+    class Program
+    {
+        // Replace sender@example.com with your "From" address.
+        // This address must be verified with Amazon SES.
+        static readonly string senderAddress = "sender@example.com";
+
+        // Replace recipient@example.com with a "To" address. If your account
+        // is still in the sandbox, this address must be verified.
+        static readonly string receiverAddress = "recipient@example.com";
+
+        // The configuration set to use for this email. If you do not want to use a
+        // configuration set, comment out the following property and the
+        // ConfigurationSetName = configSet argument below. 
+        static readonly string configSet = "ConfigSet";
+
+        // The subject line for the email.
+        static readonly string subject = "Amazon SES test (AWS SDK for .NET)";
+
+        // The email body for recipients with non-HTML email clients.
+        static readonly string textBody = "Amazon SES Test (.NET)\r\n"
+                                        + "This email was sent through Amazon SES "
+                                        + "using the AWS SDK for .NET.";
+
+        // The HTML body of the email.
+        static readonly string htmlBody = @"<html>
+<head></head>
+<body>
+  <h1>Amazon SES Test (AWS SDK for .NET)</h1>
+  <p>This email was sent with
+    <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+    <a href='https://aws.amazon.com/sdk-for-net/'>
+      AWS SDK for .NET</a>.</p>
+</body>
+</html>";
+
+        static void Main(string[] args)
+        {
+            // Replace USWest2 with the AWS Region you're using for Amazon SES.
+            // Acceptable values are EUWest1, USEast1, and USWest2.
+            using (var client = new AmazonSimpleEmailServiceClient(RegionEndpoint.USWest2))
+            {
+                var sendRequest = new SendEmailRequest
+                {
+                    Source = senderAddress,
+                    Destination = new Destination
+                    {
+                        ToAddresses =
+                        new List<string> { receiverAddress }
+                    },
+                    Message = new Message
+                    {
+                        Subject = new Content(subject),
+                        Body = new Body
+                        {
+                            Html = new Content
+                            {
+                                Charset = "UTF-8",
+                                Data = htmlBody
+                            },
+                            Text = new Content
+                            {
+                                Charset = "UTF-8",
+                                Data = textBody
+                            }
+                        }
+                    },
+                    // If you are not using a configuration set, comment
+                    // or remove the following line 
+                    ConfigurationSetName = configSet
+                };
+                try
+                {
+                    Console.WriteLine("Sending email using Amazon SES...");
+                    var response = client.SendEmail(sendRequest);
+                    Console.WriteLine("The email was sent successfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("The email was not sent.");
+                    Console.WriteLine("Error message: " + ex.Message);
+
+                }
+            }
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
